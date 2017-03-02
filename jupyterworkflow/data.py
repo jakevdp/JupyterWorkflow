da82499227ae4371529ccf3b41cdc34622e09cb8
Jake VanderPlas <jakevdp@gmail.com>
2017-03-02 11:02:22 -0800

    BUG: fix bug in date parsing

--- jupyterworkflow/data.py
+++ jupyterworkflow/data.py
@@ -29,7 +29,7 @@ def get_fremont_data(filename='Fremont.csv', url=FREMONT_URL,
     data = pd.read_csv('Fremont.csv', index_col='Date')
 
     try:
-        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %H:%M:%S %p')
+        data.index = pd.to_datetime(data.index, format='%m/%d/%Y %I:%M:%S %p')
     except TypeError:
         data.index = pd.to_datetime(data.index)
 
--- jupyterworkflow/tests/test_data.py
+++ jupyterworkflow/tests/test_data.py
@@ -1,7 +1,10 @@
 from jupyterworkflow.data import get_fremont_data
 import pandas as pd
+import numpy as np
+
 
 def test_fremont_data():
     data = get_fremont_data()
     assert all(data.columns == ['West', 'East', 'Total'])
     assert isinstance(data.index, pd.DatetimeIndex)
+    assert len(np.unique(data.index.time) == 24)
